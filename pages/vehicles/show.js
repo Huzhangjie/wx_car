ee72829c2041e81d895dcf86e21eb291231769f3
Huzhangjie <1534634433@qq.com>
2017-11-23 23:30:49 +0800

    使用entity获取详情的数据 在onload中使用options 获得传递来的数据

--- pages/vehicles/show.js
+++ pages/vehicles/show.js
@@ -1,8 +1,22 @@
+const app = getApp();
+
 Page({
     data: {
-
+        entity: null,
     },
-    onLoad() {
+    onLoad(options) {
+        const id = options.id;
+        // console.log(id);
+        const entity = app.globalData.vehicles.filter(vehicle => {
+            return id == vehicle.id;
+        });
         
+        // console.log(entity);
+        this.setData({
+            entity: entity[0],
+        });
+        wx.setNavigationBarTitle({
+            title: this.data.entity.header,
+        })
     }
 })
