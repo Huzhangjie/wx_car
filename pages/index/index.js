73f948df3264187c037d0f2bf9e39cf39f843d62
Huzhangjie <1534634433@qq.com>
2017-11-23 23:20:57 +0800

    引入testDriver 和 const app 以获取全局属性 添加 readMore方法使用igateTo转换页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,30 @@
+
+import testDrive from '../../modules/test-drive';
+console.log(testDrive);
+
 const app = getApp()
 
 Page({
   data: {
     slides: null,
   },
+  readMore: function(e) {
+    let id = e.target.dataset.id;
+    wx.navigateTo({
+      // params queryString 查询字符串 ?id 根据id 传到目标页面
+      url: `/pages/vehicles/show?id=${id}`,
+      
+    })
+  },
+
   onLoad () {
     // 数据来源还可以全局数据  globalData
     // console.log(app.globalData.slides);
     this.setData({
       slides: app.globalData.slides,
     })
-  }
+  },
+
+  testDrive,
+
 })
